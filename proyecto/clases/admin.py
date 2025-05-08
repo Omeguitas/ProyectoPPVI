365a17d1ea8a1a1ef7544e5bbbb2666fa28c43a9
Omeguitas <guillermo.fullstack@gmail.com>
2025-05-08 15:29:46 -0300

    preAdmin

---
+++ proyecto/clases/admin.py
@@ -0,0 +1,12 @@
+class Admin:
+    def __init__(self,username:str,password:str,superUser:bool=False):
+        self.__username = username
+        self.__pasword = password
+        self.__superUser = superUser
+
+
+    def save(self):
+        pass
+
+    def addUnity(self,unity):
+        pass
