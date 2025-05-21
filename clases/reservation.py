44e64558148cd0cfa0b2a117355483e9803bdde2
Omeguitas <guillermo.fullstack@gmail.com>
2025-05-21 19:00:06 -0300

    intermedio

--- clases/reservation.py
+++ clases/reservation.py
@@ -1,3 +1,16 @@
+from datetime import date
+
 class Reservation:
-    def __init__(self):
+    def __init__(self, unit_id: int, guest_id: int, check_in_date: date, check_out_date: date, price: float, amount_paid: float, DB, id:int = None):
+        self.unit_id = unit_id
+        self.guest_id = guest_id
+        self.check_in_date = check_in_date
+        self.check_out_date = check_out_date
+        self.price = price
+        self.amount_paid = amount_paid
+        self.id = id
+        self.DB = DB
+
+    def save(self):
+        
         pass
